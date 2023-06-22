b52874af2382ef6474e228a1120d4c45a2f98b62
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-06-22 15:17:04 +0530

    todoServer assignment done!

--- 02-nodejs/todoServer.js
+++ 02-nodejs/todoServer.js
@@ -39,11 +39,64 @@
 
   Testing the server - run `npm run test-todoServer` command in terminal
  */
-const express = require('express');
-const bodyParser = require('body-parser');
-
+const express = require("express");
 const app = express();
 
-app.use(bodyParser.json());
+const bodyParser = require("body-parser");
 
+const port = 3001;
+
+app.use(bodyParser.json());
+app.get("/todos", (req, res) => {
+  res.json(allTodoItems);
+  res.status(200).send();
+});
+app.get(`/todos/:id`, (req, res) => {
+  const todo = allTodoItems.find((item) => item.id === parseInt(req.params.id));
+  if (!todo) {
+    res.status(404).send();
+  } else {
+    res.json(todo);
+  }
+});
+app.delete("/todos/:id", (req,res)=>{
+  const todoIndex = allTodoItems.findIndex((item) => item.id === parseInt(req.params.id));
+  if (todoIndex=== -1) {
+    res.status(404).send();
+  }else{
+    allTodoItems.splice(todoIndex, 1);
+    res.status(200).send();
+  }
+})
+app.put('/todos/:id', (req, res) => {
+  const todoIndex = allTodoItems.findIndex(t => t.id === parseInt(req.params.id));
+  if (todoIndex === -1) {
+    res.status(404).send();
+  } else {
+    allTodoItems[todoIndex].title = req.body.title;
+    allTodoItems[todoIndex].description = req.body.description;
+    res.json(allTodoItems[todoIndex]);
+  }
+});
+
+app.post("/todos", (req,res)=>{
+  const itemToPush ={};
+  itemToPush.id = Math.floor(Math.random() * 1000000);
+  console.log("id generated and saved")
+  itemToPush.title = req.body.title;
+  itemToPush.description = req.body.description;
+  itemToPush.completed = req.body.completed;
+  res.status(201).json(itemToPush);
+  allTodoItems.push(itemToPush);
+})
+
+const allTodoItems = [];
+
+app.use((req,res,next) =>{
+  res.status(404).send();
+})
+
+app.listen(port, () => {
+  console.log(`Example app is listening on port ${port}`);
+});
 module.exports = app;
